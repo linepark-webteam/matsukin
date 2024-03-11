76416eb88b56d44dadecf1cf438e03522f33501c
DESKTOP-2QJH1HL\19 <ko.nagai.0801@gmail.com>
2024-03-11 09:42:33 +0900

    Add FooterHTML

---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer class="container-fluid d-flex justify-content-center mt-5 py-3">
+      <a href="#" id="page_top"></a>
+      <div class="container">
+        <div class="row">
+          <div class="col-6 col-lg-4">
+            <img src="./img/logo.webp" alt="企業ロゴ" class="col-12 py-3">
+          </div>
+        </div>
+        <div class="row d-flex justify-content-around">
+          <div class="col-lg-4 align-content-center">
+            <p class="content-text">東北松金工業株式会社</p>
+            <p class="content-text">
+              〒981-4122<br>
+              宮城県加美郡色麻町<br>
+              四竃字大原225-3<br>
+            </p>
+            <p class="content-text">TEL: <a href="tel:0229-66-1111">0229-66-1111</a><br>FAX: 0229-66-1110<br>MAIL: <a href="mailto:k-matsumoto@t-matsukin.co.jp">k-matsumoto@t-matsukin.co.jp</a></p>
+          </div>
+          <div class="col-lg-4">
+            <!-- <p class="content-text"><a href="./philosophy/">経営理念</a></p> -->
+            <p class="content-text"><a href="./about.html">会社概要</a></p>
+            <p class="content-text"><a href="./message.html">代表メッセージ</a></p>
+            <p class="content-text"><a href="./access.html">アクセス</a></p>
+          </div>
+          <div class="col-lg-4">
+            <p class="content-text"><a href="./contact.html">お問い合わせ</a></p>
+            <p class="content-text"><a href="./privacy-policy.html">個人情報保護方針</a></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <div class="copyright container-fluid row justify-content-center align-content-center">
+      Copyright 2024 東北松金工業株式会社
+    </div>
