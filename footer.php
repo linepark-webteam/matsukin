a419d32f8e339fa0f7f503d0784e5b5898307dc7
DESKTOP-2QJH1HL\19 <ko.nagai.0801@gmail.com>
2024-03-11 10:40:11 +0900

    Add AccessPage

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
-<footer class="container-fluid d-flex justify-content-center mt-5 py-3">
-      <a href="#" id="page_top"></a>
+<a href="#" id="page_top"></a>
+    <footer class="container-fluid d-flex justify-content-center mt-5 py-3">
       <div class="container">
         <div class="row">
           <div class="col-6 col-lg-4">
             <img src="./img/logo.webp" alt="企業ロゴ" class="col-12 py-3">
           </div>
         </div>
-        <div class="row d-flex justify-content-around">
-          <div class="col-lg-4 align-content-center">
+        <div class="row d-flex justify-content-center">
+          <div class="col-lg-6 align-content-center">
             <p class="content-text">東北松金工業株式会社</p>
             <p class="content-text">
               〒981-4122<br>
@@ -16,19 +16,21 @@
             </p>
             <p class="content-text">TEL: <a href="tel:0229-66-1111">0229-66-1111</a><br>FAX: 0229-66-1110<br>MAIL: <a href="mailto:k-matsumoto@t-matsukin.co.jp">k-matsumoto@t-matsukin.co.jp</a></p>
           </div>
-          <div class="col-lg-4">
+          <div class="col-lg-3">
             <!-- <p class="content-text"><a href="./philosophy/">経営理念</a></p> -->
             <p class="content-text"><a href="./about.html">会社概要</a></p>
-            <p class="content-text"><a href="./message.html">代表メッセージ</a></p>
+            <p class="content-text"><a href="./message.html">代表挨拶</a></p>
             <p class="content-text"><a href="./access.html">アクセス</a></p>
           </div>
-          <div class="col-lg-4">
+          <div class="col-lg-3">
             <p class="content-text"><a href="./contact.html">お問い合わせ</a></p>
             <p class="content-text"><a href="./privacy-policy.html">個人情報保護方針</a></p>
           </div>
         </div>
       </div>
     </footer>
-    <div class="copyright container-fluid row justify-content-center align-content-center">
-      Copyright 2024 東北松金工業株式会社
+    <div class="container-fluid">
+      <div class="d-flex justify-content-center align-items-center py-1">
+        <small class="copyright">Copyright 2024 東北松金工業株式会社</small>
+      </div>
     </div>
