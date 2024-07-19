4312623dd6066ec5931b9d6897f28df066f7f392
ko-nagai-0801 <ko.nagai.0801@gmail.com>
2024-07-19 09:42:20 +0900

    edit animation

--- js/script.js
+++ js/script.js
@@ -1,74 +1,73 @@
 console.log("test");
 
-
 // TOP HERO SVGアニメーション
 console.log("test");
 
 // Vivusの描画と同時にAnime.jsのアニメーションを開始
 function startVivusAnimation() {
   // Vivusインスタンスを作成
-  new Vivus('house', {
+  new Vivus("house", {
     duration: 200,
-    type: 'async',
+    type: "async",
     animTimingFunction: Vivus.EASE,
-    start: 'inViewport', // ページに入った時にアニメーションを開始
+    start: "inViewport", // ページに入った時にアニメーションを開始
     onReady: function (obj) {
       // 描画開始時にAnime.jsのアニメーションを開始
       anime({
-        targets: '#house path',
-        translateX: function() {
-          return anime.random(-10, 10) + 'px';
+        targets: "#house path",
+        translateX: function () {
+          return anime.random(-10, 10) + "px";
         },
-        translateY: function() {
-          return anime.random(-10, 10) + 'px';
+        translateY: function () {
+          return anime.random(-10, 10) + "px";
         },
-        easing: 'linear',
+        easing: "linear",
         duration: 1000,
-        direction: 'alternate',
-        loop: true
+        direction: "alternate",
+        loop: true,
       });
     },
     onEnd: function (obj) {
-      obj.el.classList.add('finished'); // アニメーション完了を示すクラスを追加
-    }
+      obj.el.classList.add("finished"); // アニメーション完了を示すクラスを追加
+    },
   });
 }
 
-document.addEventListener('DOMContentLoaded', startVivusAnimation);
-
+document.addEventListener("DOMContentLoaded", startVivusAnimation);
 
 // TOPページ SVG背景アニメーション
 // 初期設定
 let duration = window.innerWidth < 992 ? 10 : 200;
 
 // Vivusインスタンスを作成
-let vivus = new Vivus('my-svg', {
-  type: 'delayed',
+let vivus = new Vivus("my-svg", {
+  type: "delayed",
   duration: duration,
-  start: 'manual', // 手動でアニメーションを開始
-  dashGap: 20
+  start: "manual", // 手動でアニメーションを開始
+  dashGap: 20,
 });
 
 // Anime.jsのアニメーション設定
 let animeInstance = anime({
-  targets: '#my-svg path',
-  translateX: function() {
-    return anime.random(-5, 5) + 'px';
+  targets: "#my-svg path",
+  translateX: function () {
+    return anime.random(-5, 5) + "px";
   },
-  translateY: function() {
-    return anime.random(-5, 5) + 'px';
+  translateY: function () {
+    return anime.random(-5, 5) + "px";
   },
-  easing: 'easeInOutSine',
+  easing: "easeInOutSine",
   duration: 1000,
-  direction: 'alternate',
+  direction: "alternate",
   loop: true,
-  autoplay: false // 自動再生をオフにする
+  autoplay: false, // 自動再生をオフにする
 });
 
 // スクロールイベントに基づいて描画を制御
-document.addEventListener('scroll', function() {
+document.addEventListener("scroll", function () {
   let scrollPosition = window.scrollY;
-  let documentHeight = document.documentElement.scrollHeight - window.innerHeight;
+  let documentHeight =
+    document.documentElement.scrollHeight - window.innerHeight;
   let progress = scrollPosition / documentHeight;
 
   // Vivusの進行状況を設定
@@ -79,22 +78,23 @@ document.addEventListener('scroll', function() {
 });
 
 // ウィンドウサイズ変更時のリスナーを追加して、必要に応じて再設定
-window.addEventListener('resize', function() {
+window.addEventListener("resize", function () {
   let newDuration = window.innerWidth < 992 ? 10 : 200;
   if (newDuration !== duration) {
     duration = newDuration;
     // Vivusインスタンスを再作成して設定を更新
-    vivus = new Vivus('my-svg', {
-      type: 'delayed',
+    vivus = new Vivus("my-svg", {
+      type: "delayed",
       duration: duration,
-      start: 'manual', // 手動でアニメーションを開始
-      dashGap: 20
+      start: "manual", // 手動でアニメーションを開始
+      dashGap: 20,
     });
 
     // スクロールイベントに基づいて描画を制御
-    document.addEventListener('scroll', function() {
+    document.addEventListener("scroll", function () {
       let scrollPosition = window.scrollY;
-      let documentHeight = document.documentElement.scrollHeight - window.innerHeight;
+      let documentHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
       let progress = scrollPosition / documentHeight;
 
       // Vivusの進行状況を設定
@@ -106,6 +106,40 @@ window.addEventListener('resize', function() {
   }
 });
 
+// テキストを一文字ずつ表示するアニメーション
+// テキストを一文字ずつ分割してspanタグで囲む
+function splitTextIntoSpans(element) {
+  const text = element.textContent;
+  element.innerHTML = '';
+  text.split('').forEach((char) => {
+    const span = document.createElement('span');
+    span.textContent = char === ' ' ? '\u00A0' : char; // 半角スペースを &nbsp; に置き換える
+    span.classList.add('letter');
+    element.appendChild(span);
+  });
+}
+
+// アニメーションを適用する
+function animateText() {
+  const textElements = document.querySelectorAll('.animate-text');
+
+  textElements.forEach((element, index) => {
+    splitTextIntoSpans(element);
+
+    anime.timeline({loop: false})
+      .add({
+        targets: element.querySelectorAll('.letter'),
+        opacity: [0, 1],
+        easing: 'easeInOutQuad',
+        duration: 750,
+        delay: (el, i) => 50 * (i + 1),
+        offset: index * 1000 // 各要素のアニメーションの開始をずらす
+      });
+  });
+}
+
+// ページが読み込まれたときにアニメーションを開始
+document.addEventListener('DOMContentLoaded', animateText);
 
 
 // プリローダーアニメーション
@@ -142,112 +176,112 @@ window.onload = function () {
     });
   });
 
-// Swipperライブラリ(スライドアニメーション)の為のScript
-const swiper1 = new Swiper('.swiper-container.top', {
-  loop: true,
-  spaceBetween: 50,
-  speed: 5000,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  allowTouchMove: true,
-  breakpoints: {
-    320: {
-      slidesPerView: 2.5,
+  // Swipperライブラリ(スライドアニメーション)の為のScript
+  const swiper1 = new Swiper(".swiper-container.top", {
+    loop: true,
+    spaceBetween: 50,
+    speed: 5000,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
     },
-    480: {
-      slidesPerView: 2.5,
+    allowTouchMove: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 2.5,
+      },
+      480: {
+        slidesPerView: 2.5,
+      },
+      600: {
+        slidesPerView: 3.5,
+      },
+      768: {
+        slidesPerView: 3.5,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+      1200: {
+        slidesPerView: 5,
+      },
     },
-    600: {
-      slidesPerView: 3.5,
-    },
-    768: {
-      slidesPerView: 3.5,
-    },
-    1024: {
-      slidesPerView: 4,
-    },
-    1200: {
-      slidesPerView: 5,
-    }
-  }
-});
+  });
 
-const swiper2 = new Swiper('.swiper-container.products', {
-  loop: true,
-  spaceBetween: 25,
-  speed: 5000,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  allowTouchMove: true,
-  breakpoints: {
-    320: {
-      slidesPerView: 1.5,
+  const swiper2 = new Swiper(".swiper-container.products", {
+    loop: true,
+    spaceBetween: 25,
+    speed: 5000,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
     },
-    480: {
-      slidesPerView: 2,
+    allowTouchMove: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1.5,
+      },
+      480: {
+        slidesPerView: 2,
+      },
+      600: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 3.5,
+      },
+      1024: {
+        slidesPerView: 3.5,
+      },
+      1200: {
+        slidesPerView: 4.5,
+      },
     },
-    600: {
-      slidesPerView: 3,
+  });
+  const swiper3 = new Swiper(".swiper-container.works", {
+    loop: true,
+    spaceBetween: 25,
+    speed: 5000,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
     },
-    768: {
-      slidesPerView: 3.5,
+    allowTouchMove: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1.5,
+      },
+      480: {
+        slidesPerView: 2,
+      },
+      600: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 3.5,
+      },
+      1024: {
+        slidesPerView: 3.5,
+      },
+      1200: {
+        slidesPerView: 4.5,
+      },
     },
-    1024: {
-      slidesPerView: 3.5,
-    },
-    1200: {
-      slidesPerView: 4.5,
-    }
-  }
-});
-const swiper3 = new Swiper('.swiper-container.works', {
-  loop: true,
-  spaceBetween: 25,
-  speed: 5000,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  allowTouchMove: true,
-  breakpoints: {
-    320: {
-      slidesPerView: 1.5,
-    },
-    480: {
-      slidesPerView: 2,
-    },
-    600: {
-      slidesPerView: 3,
-    },
-    768: {
-      slidesPerView: 3.5,
-    },
-    1024: {
-      slidesPerView: 3.5,
-    },
-    1200: {
-      slidesPerView: 4.5,
-    }
-  }
-});
-}
+  });
+};
 
-  // スライドショーの初期化と設定
-  // const slides = document.querySelectorAll(".slide");
-  // let index = 0;
+// スライドショーの初期化と設定
+// const slides = document.querySelectorAll(".slide");
+// let index = 0;
 
-  // function changeSlide() {
-  //   slides.forEach((slide) => slide.classList.remove("active"));
-    // すべてのスライドから 'active' クラスを削除
-    // slides[index].classList.add("active"); // 現在のスライドに 'active' クラスを追加
-    // index = (index + 1) % slides.length;
-    // 次のスライドのインデックスを計算
-  // }
+// function changeSlide() {
+//   slides.forEach((slide) => slide.classList.remove("active"));
+// すべてのスライドから 'active' クラスを削除
+// slides[index].classList.add("active"); // 現在のスライドに 'active' クラスを追加
+// index = (index + 1) % slides.length;
+// 次のスライドのインデックスを計算
+// }
 
-  // 5秒ごとにスライドを切り替え
+// 5秒ごとにスライドを切り替え
 //   setInterval(changeSlide, 5000);
 // };
